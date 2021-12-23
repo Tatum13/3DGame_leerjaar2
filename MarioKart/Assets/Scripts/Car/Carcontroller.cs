cc7f9017a66b5c33afdc72236f425938af631e1e
Bas <30810@ma-web.nl>
2021-12-23 12:52:08 +0100

    Je kan sturen en gravity is groter als je in de lucht bent

--- MarioKart/Assets/Scripts/Car/Carcontroller.cs
+++ MarioKart/Assets/Scripts/Car/Carcontroller.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Carcontroller : MonoBehaviour
 {
+    /// <summary>
+    /// Hiermee kan je gas geven, achter uit rijden en sturen.
+    /// Ook laat ik de gravity groter zijn als je in de lucht bent.
+    /// En de wielen draaien als je vertical input doet.
+    /// </summary>
+
     public Rigidbody RB;
 
-    public float forwardAccel = 2f, reverseAccel = 1f, maxSpeed = 10f, trunStrenght = 90f;
+    public float forwardAccel = 2f, reverseAccel = 1f, maxSpeed = 10f, turnStrenght = 90f, gravityForce = 10f, dragOnGround = 0f;
 
     private float speedInput, turnInput;
+
+    private bool grounded;
+
+    public LayerMask wahtIsGround;
+    public float gourndRayLength = .5f;
+    public Transform groundRayPoint;
+
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTrun = 40;
 
     void Start()
     {
@@ -20,21 +35,48 @@ public class Carcontroller : MonoBehaviour
         speedInput = 0;
         if(Input.GetAxis("Vertical") > 0)
         {
-            speedInput = Input.GetAxis("Vertical") * forwardAccel * 50f;
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 25f;
         }
         else if (Input.GetAxis("Vertical") < 0)
         {
-            speedInput = Input.GetAxis("Vertical") * reverseAccel * 50f;
+            speedInput = Input.GetAxis("Vertical") * reverseAccel * 25f;
         }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        if(grounded)
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrenght * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTrun) - 180, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTrun, rightFrontWheel.localRotation.eulerAngles.z);
 
         transform.position = RB.transform.position;
     }
 
     private void FixedUpdate()
     {
-        if(Mathf.Abs(speedInput) > 0)
+        grounded = false;
+        RaycastHit hit;
+
+        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, gourndRayLength, wahtIsGround))
         {
-            RB.AddForce(transform.forward * speedInput);
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        if (grounded)
+        {
+            RB.drag = dragOnGround;
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                RB.AddForce(transform.forward * speedInput);
+            }
+        }
+        else
+        {
+            RB.drag = 0.1f;
+            RB.AddForce(Vector3.up * -gravityForce * 5f);
         }
     }
 }
